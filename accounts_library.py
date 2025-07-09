678ed3e657f5a1ff204b75af42e775c1bd172315
snowflake-chan <lawrence.bi@icloud.com>
2025-07-09 20:40:33 +0800

    Finished basic architecture

---
+++ accounts_library.py
@@ -0,0 +1,48 @@
+import sqlite3
+from user import User
+
+class AccountManager:
+    def __init__(self, lib=".db"):
+        """
+        Initialize the account manager
+        :param lib: Path to the sqlite3 database
+        """
+        self.con = sqlite3.connect(lib)
+        self._create_table()
+
+    def _create_table(self):
+        """ Create the table if it doesn't exist """
+
+        table_sql = """
+        CREATE TABLE IF NOT EXISTS accounts (
+            id INTEGER PRIMARY KEY,
+            identity TEXT,
+            nickname TEXT,
+            password TEXT,
+            token TEXT,
+            has_phone_number INTEGER,
+            comments TEXT,
+            last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+        """
+        self.con.execute(table_sql)
+        self.con.commit()
+
+        trigger_sql = """
+        CREATE TRIGGER IF NOT EXISTS update_timestamp
+        AFTER UPDATE ON accounts
+        FOR EACH ROW
+        BEGIN
+            UPDATE accounts SET last_updated = CURRENT_TIMESTAMP WHERE uid = OLD.uid;
+        END;
+        """
+        self.con.execute(trigger_sql)
+        self.con.commit()
+
+    def add_account(self,identity,password):
+        user = User(identity,password)
+        has_phone_number = user.phone_number is not None
+        self.con.execute("INSERT INTO accounts(id,identity,nickname,password,token,has_phone_number,comments,last_updated)"
+                         "VALUES (?,?,?,?,?,?,'',CURRENT_TIMESTAMP);",
+                         (user.id, identity, user.nickname, password, user.token, has_phone_number))
+        self.con.commit()
---
+++ main.py
@@ -0,0 +1,51 @@
+from PyInquirer import prompt
+
+from accounts_library import AccountManager
+
+if __name__ == '__main__':
+    manager = AccountManager()
+
+    while True:
+        try:
+            main_list = [
+                {
+                    'type': 'list',
+                    'name': 'action',
+                    'message': 'nekoflow',
+                    'choices': [
+                        'Add Account',
+                        'Help',
+                        'Exit'
+                    ]
+                }
+            ]
+
+            action = prompt(main_list)['action']
+
+            if action == 'Add Account':
+                questions = [
+                    {
+                        'type': 'input',
+                        'name': 'identity',
+                        'message': 'identity> '
+                    },
+                    {
+                        'type': 'input',
+                        'name': 'password',
+                        'message': 'password> '
+                    }
+                ]
+                identity, password = prompt(questions).values()
+                manager.add_account(identity, password)
+
+            elif action == 'About':
+                print('nekoflow(R) 2025. All rights reserved.')
+
+            elif action == 'Exit':
+                exit(0)
+
+        except KeyboardInterrupt:
+            exit(0)
+
+        except Exception as e:
+            print(e)
